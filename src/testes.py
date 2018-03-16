571d9de0005cd4a8d22c9c42fab30547ee416cb7
ViniciusSilvaCosta <36552435+ViniciusSilvaCosta@users.noreply.github.com>
2018-03-16 20:30:44 -0300

    Update testes.py

--- src/testes.py
+++ src/testes.py
@@ -5,14 +5,15 @@ erroInicializar = false
 jogo = jogovelha.inicializar()
 
 if len(jogo) !=3:
-  else:
-    for linha in jogo:
+  erroInicializar = true
+else:
+  for linha in jogo:
       if len(linha) != 3:
-        erroInicializar = true
+          erroInicializar = true
       else:
         for elemento in linha:
           if elemento != '.':
-            erroInicializar = true
+              erroInicializar = true
 if erroInicializar:
   sys.exit(1)
 else:
